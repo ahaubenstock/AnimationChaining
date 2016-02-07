460b22a89a5d2b3a4b48eca3160f00e79dc993f6
Adam Haubenstock <ahaubenstock@hanekedesign.com>
2016-02-07 01:27:25 -0500

    Initial Commit

---
+++ AnimationChaining/AnimationChaining.h
@@ -0,0 +1,19 @@
+//
+//  AnimationChaining.h
+//  AnimationChaining
+//
+//  Created by Adam E. Haubenstock on 2/7/16.
+//  Copyright © 2016 Adam E. Haubenstock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimationChaining.
+FOUNDATION_EXPORT double AnimationChainingVersionNumber;
+
+//! Project version string for AnimationChaining.
+FOUNDATION_EXPORT const unsigned char AnimationChainingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimationChaining/PublicHeader.h>
+
+
